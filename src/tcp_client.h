bf96c9df085dd8ca193e0c53461a51495d6605f6
Philip Lundrigan <philiplundrigan@gmail.com>
2021-08-27 12:30:32 -0600

    Update interface

--- src/tcp_client.h
+++ src/tcp_client.h
@@ -17,61 +17,117 @@
 #define TCP_CLIENT_BAD_SOCKET -1
 #define TCP_CLIENT_DEFAULT_PORT "8082"
 #define TCP_CLIENT_DEFAULT_HOST "localhost"
-#define TCP_CLIENT_MAX_LINE_SIZE 1024
 #define TCP_CLIENT_REQUEST_HEADER_SIZE 4
 #define TCP_CLIENT_RESPONSE_HEADER_SIZE 4
 
-// Contains all of the information needed to create to connect to the server and
-// send it a message.
+/*
+Contains all of the information needed to create to connect to the server and send it a message.
+*/
 typedef struct Config {
     char *port;
     char *host;
     char *file;
 } Config;
 
-// Parses the options given to the program. It will return a Config struct with the necessary
-// information filled in. argc and argv are provided by main. If an error occurs in processing the
-// arguments and options (such as an invalid option), this function will print the correct message
-// and then exit.
-Config tcp_client_parse_arguments(int argc, char *argv[]);
+/*
+Description:
+    Parses the commandline arguments and options given to the program.
+Arguments:
+    int argc: the amount of arguments provided to the program (provided by the main function)
+    char *argv[]: the array of arguments provided to the program (provided by the main function)
+    Config *config: An empty Config struct that will be filled in by this function.
+Return value:
+    Returns a 1 on failure, 0 on success
+*/
+int tcp_client_parse_arguments(int argc, char *argv[], Config *config);
 
-////////////////////////////////////////////////////
-///////////// SOCKET RELATED FUNCTIONS /////////////
-////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+/////////////////////// SOCKET RELATED FUNCTIONS //////////////////////
+///////////////////////////////////////////////////////////////////////
 
-// Creates a TCP socket and connects it to the specified host and port. It returns the socket file
-// descriptor.
+/*
+Description:
+    Creates a TCP socket and connects it to the specified host and port.
+Arguments:
+    Config config: A config struct with the necessary information.
+Return value:
+    Returns the socket file descriptor or -1 if an error occurs.
+*/
 int tcp_client_connect(Config config);
 
-// Using the the action and message provided by the command line, format the data to follow the
-// protocol and send it to the server. Return 1 (EXIT_FAILURE) if an error occurs, otherwise return
-// 0 (EXIT_SUCCESS).
+/*
+Description:
+    Creates and sends request to server using the socket and configuration.
+Arguments:
+    int sockfd: Socket file descriptor
+    char *action: The action that will be sent
+    char *message: The message that will be sent
+Return value:
+    Returns a 1 on failure, 0 on success
+*/
 int tcp_client_send_request(int sockfd, char *action, char *message);
 
-// Receive the response from the server. The caller must provide a function pointer that handles the
-// response and returns a true value if all responses have been handled, otherwise it returns a
-// false value. After the response is handled by the handle_response function pointer, the response
-// data can be safely deleted. The string passed to the function pointer must be null terminated.
-// Return 1 (EXIT_FAILURE) if an error occurs, otherwise return 0 (EXIT_SUCCESS).
+/*
+Description:
+    Receives the response from the server. The caller must provide a function pointer that handles
+the response and returns a true value if all responses have been handled, otherwise it returns a
+    false value. After the response is handled by the handle_response function pointer, the response
+    data can be safely deleted. The string passed to the function pointer must be null terminated.
+Arguments:
+    int sockfd: Socket file descriptor
+    int (*handle_response)(char *): A callback function that handles a response
+Return value:
+    Returns a 1 on failure, 0 on success
+*/
 int tcp_client_receive_response(int sockfd, int (*handle_response)(char *));
 
-// Close the socket when your program is done running.
-void tcp_client_close(int sockfd);
+/*
+Description:
+    Closes the given socket.
+Arguments:
+    int sockfd: Socket file descriptor
+Return value:
+    Returns a 1 on failure, 0 on success
+*/
+int tcp_client_close(int sockfd);
 
-////////////////////////////////////////////////////
-////////////// FILE RELATED FUNCTIONS //////////////
-////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+//////////////////////// FILE RELATED FUNCTIONS ///////////////////////
+///////////////////////////////////////////////////////////////////////
 
-// Given a file name, open that file for reading, returning the file object.
+/*
+Description:
+    Opens a file.
+Arguments:
+    char *file_name: The name of the file to open
+Return value:
+    Returns NULL on failure, a FILE pointer on success
+*/
 FILE *tcp_client_open_file(char *file_name);
 
-// Gets the next line of a file, filling in action and message. This function should be similar
-// design to getline() (https://linux.die.net/man/3/getline). *action and message must be allocated
-// by the function and freed by the caller.* When this function is called, action must point to the
-// action string and the message must point to the message string.
+/*
+Description:
+    Gets the next line of a file, filling in action and message. This function should be similar
+    design to getline() (https://linux.die.net/man/3/getline). *action and message must be allocated
+    by the function and freed by the caller.* When this function is called, action must point to the
+    action string and the message must point to the message string.
+Arguments:
+    FILE *fd: The file pointer to read from
+    char **action: A pointer to the action that was read in
+    char **message: A pointer to the message that was read in
+Return value:
+    Returns -1 on failure, the number of characters read on success
+*/
 int tcp_client_get_line(FILE *fd, char **action, char **message);
 
-// Close the file when your program is done with the file.
-void tcp_client_close_file(FILE *fd);
+/*
+Description:
+    Closes a file.
+Arguments:
+    FILE *fd: The file pointer to close
+Return value:
+    Returns a 1 on failure, 0 on success
+*/
+int tcp_client_close_file(FILE *fd);
 
 #endif
